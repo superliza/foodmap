6b51c9dd00c80e34260f78913c5d4d47d47a61f3
superliza <eli_darko@hotmail.com>
2018-05-04 21:02:27 -0500

    filtrado de restaurantes

--- js/data.js
+++ js/data.js
@@ -1,6 +1,7 @@
-var data = {
+let data = {
    'restaurants' : [
-       {	
+       {   
+           'id': 1,
        	   'name' : 'Silvana',
            'address' : 'Acueducto #5098 Xochimilco, CDMX',
            'food' : 'Mexicana',
@@ -8,7 +9,8 @@ var data = {
            'serviceHome' : true,
            'photo' : "http://lorempixel.com/350/200/food/"
        },
-       {
+       {   
+           'id': 2,
        	   'name' : 'Jonh',
            'address' : 'Halcon #4059 Xochimilco, CDMX',          
            'food' : 'Italiana',
@@ -16,7 +18,8 @@ var data = {
            'serviceHome' : false,
            'photo' : "http://lorempixel.com/350/200/food/"
        },
-       {
+       {    
+           'id': 3,
        	   'name' : 'Lucile',
            'address' : 'Buenavista #2040 Cuahutémoc, CDMX',          
            'food' : 'Francesa',
@@ -25,6 +28,7 @@ var data = {
            'photo' : "http://lorempixel.com/350/200/food/"
        },
        {
+           'id': 4,
        	   'name' : 'Amalia',
            'address' : 'Centro de Tlalpan #2040 Tlalpan, CDMX',          
            'food' : 'Japonesa',
@@ -32,13 +36,23 @@ var data = {
            'serviceHome' : false,
            'photo' : "http://lorempixel.com/350/200/food/"
        },
+       {    
+            'id': 5,
+            'name' : 'Manu',
+            'address' : 'Divisón del Norte #2040 Coyoacán, CDMX',          
+            'food' : 'Alemana',
+            'menu' : ['Codillo de cerdo', 'Quiche', 'Brunch'],
+            'serviceHome' : true,
+            'photo' : "http://lorempixel.com/350/200/food/"
+       },
        {
-       	   'name' : 'Manu',
-           'address' : 'Divisón del Norte #2040 Coyoacán, CDMX',          
-           'food' : 'Alemana',
-           'menu' : ['Codillo de cerdo', 'Quiche', 'Brunch'],
-           'serviceHome' : true,
-           'photo' : "http://lorempixel.com/350/200/food/"
-       }
+            'id': 6,
+            'name' : 'Inti',
+            'address' : 'Divisón del Norte #2040 Coyoacán, CDMX',          
+            'food' : 'Vegana',
+            'menu' : ['Codillo de cerdo', 'Quiche', 'Brunch'],
+            'serviceHome' : true,
+            'photo' : "http://lorempixel.com/350/200/food/"
+        }
    ]
 };
